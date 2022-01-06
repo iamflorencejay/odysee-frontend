435dc96ee6f0fd943cb6b00f8f1f61e96a9e2528
infinite-persistence <inf.persistence@gmail.com>
2022-01-06 23:00:55 +0800

    -- tmp: warn account will be merged

--- ui/app.js
+++ ui/app.js
@@ -14,6 +14,8 @@ const app = {
 
 global.app = app;
 
+alert('This branch will migrate your odysee account to SSO.\nClose the tab now if you do not want that to happen yet.');
+
 // Lbryinc needs access to the redux store for dispatching auth-releated actions
 global.store = app.store;
 
