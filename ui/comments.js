64e65c1098695148acf8a33a0c550f3e44bdb712
infinite-persistence <inf.persistence@gmail.com>
2022-01-07 15:22:16 +0800

    Commentron: add bearer token
    Not sure if this is the best way, but it's the easiest for now.

--- ui/comments.js
+++ ui/comments.js
@@ -1,5 +1,6 @@
 // @flow
 import { COMMENT_SERVER_API } from 'config';
+import { getTokens } from 'util/saved-passwords';
 
 const Comments = {
   url: COMMENT_SERVER_API,
@@ -45,11 +46,14 @@ function fetchCommentsApi(method: string, params: {}) {
     return Promise.reject('Comments are not currently enabled.'); // eslint-disable-line
   }
 
+  // $FlowFixMe: null Comments.url already handled above.
   const url = `${Comments.url}?m=${method}`;
+  const tokens = getTokens();
   const options = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
+      ...(tokens.access_token ? { Authorization: `Bearer ${tokens.access_token}` } : {}),
     },
     body: JSON.stringify({
       jsonrpc: '2.0',
